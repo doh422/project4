f0ccaaabb193aa1c6fe387a0bde7c44e39decc81
Daniel Oh <doh422@gmail.com>
2015-12-03 15:42:12 -0800

    watchlist-service created

---
+++ app/scripts/services/watchlist-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name finalProjectApp.WatchlistService
+ * @description
+ * # WatchlistService
+ * Service in the finalProjectApp.
+ */
+angular.module('finalProjectApp')
+  .service('WatchlistService', function WatchlistService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
